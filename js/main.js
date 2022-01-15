59fdc0ff2db8f7a14e054b32ed9bcae1ff778eab
Matty <matty258987@gmail.com>
2022-01-15 17:56:40 +0000

    cleanup and fix

--- js/main.js
+++ js/main.js
@@ -2,26 +2,13 @@ const csvFile = ("data/y1s2_sa.csv");
 const reader = new FileReader();
 
 reader.onload = function (e) {
-    const text = e.target.result;
-    const data = csvToArray(text);
+    const data = csvToArray(csvFile);
     console.log(data);
   };
 
-
 function csvToArray(str, delimiter = ",") {
-    // slice from start of text to the first \n index
-    // use split to create an array from string by delimiter
     const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-  
-    // slice from \n index + 1 to the end of the text
-    // use split to create an array of each csv value row
     const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-  
-    // Map the rows
-    // split values from each row into an array
-    // use headers.reduce to create an object
-    // object properties derived from headers:values
-    // the object passed as an element of the array
     const arr = rows.map(function (row) {
       const values = row.split(delimiter);
       const el = headers.reduce(function (object, header, index) {
@@ -30,8 +17,6 @@ function csvToArray(str, delimiter = ",") {
       }, {});
       return el;
     });
-  
-    // return the array
     return arr;
   }
 
