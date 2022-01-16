f2cc46a6f09f7db46984d1eca329b1eef1dda0d9
Matty <matty258987@gmail.com>
2022-01-16 14:09:04 +0000

    formatting

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function csvToArray(str, delimiter = ",") {
   const headers = str.slice(0, str.indexOf("\n").split(delimiter));
   const rows = str.slice(str.indexOf("\n") + 1).split("\n");
 
-  const arr = rows.map(function (row){
+  const arr = rows.map(function (row) {
     const values = row.split(delimiter);
     const el = headers.reduce(function (object, header, index) {
       object[header] = values[index];
