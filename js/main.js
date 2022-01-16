8cd4ab00d4274c375a49015ffbef5571cd21a13b
Matty <matty258987@gmail.com>
2022-01-16 14:08:35 +0000

    add conversion of csv to array of objects

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 const y1s2_assessment_data = "data/y1s2_sa.csv";
 
+function csvToArray(str, delimiter = ",") {
+  const headers = str.slice(0, str.indexOf("\n").split(delimiter));
+  const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+
+  const arr = rows.map(function (row){
+    const values = row.split(delimiter);
+    const el = headers.reduce(function (object, header, index) {
+      object[header] = values[index];
+      return object;
+    }, {});
+    return el;
+  });
+  return arr;
+}
+
+
 function loadFile(filePath) {
   var result = null;
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open("GET", filePath, false);
   xmlHttp.send();
-  if (xmlHttp.status==200) {
+  if (xmlHttp.status == 200) {
     result = xmlHttp.responseText.split("\n");
+    console.log(csvToArray(xmlHttp.responseText));
   }
   return result;
 }
+
 
 const myDate = new Date();
 console.log(loadFile(y1s2_assessment_data));
