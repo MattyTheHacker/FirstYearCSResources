3397506d90f3f5e15c04a4776671ea7953189995
Matty <matty258987@gmail.com>
2022-01-16 01:18:30 +0000

    more testing

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
-const csvFile = "/data/y1s2_sa.csv";
-const reader = new FileReader();
-reader.readAsText(csvFile);
-
-reader.onload = function (e) {
-    const data = csvToArray(csvFile);
-    console.log(data);
-  };
-
+const csvFile = "https://mattythehacker.github.io/FirstYearCSResources/resources/CS-Module-Marks-Tracker-blank.xlsx";
+/**
 function csvToArray(str, delimiter = ",") {
     const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
     const rows = str.slice(str.indexOf("\n") + 1).split("\n");
@@ -20,6 +13,29 @@ function csvToArray(str, delimiter = ",") {
     });
     return arr;
 }
+*/
+var txtFile = new XMLHttpRequest();
+txtFile.open("GET", csvFile, true);
+txtFile.onreadystatechange = function() {
+  if (txtFile.readyState === 4) {  // Makes sure the document is ready to parse.
+    if (txtFile.status === 200) {  // Makes sure it's found the file.
+      allText = txtFile.responseText;
+      lines = txtFile.responseText.split("\n"); // Will separate each line into an array
+      console.log(allText);
+    }
+  }
+}
 
+/** 
+const reader = new FileReader();
+reader.onload = function (fileInput) {
+    const text = e.target.result;
+    const data = csvToArray(text);
+    document.write(JSON.stringify(data));
+};
+
+reader.readAsText(input);
+
+  */
 const myDate = new Date();
 console.log(myDate);
