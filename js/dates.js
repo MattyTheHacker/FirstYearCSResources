b9d55a0019d906a37562300c067be3b642956228
Matty <matty258987@gmail.com>
2022-06-16 00:29:29 +0100

    add table headers

--- js/dates.js
+++ js/dates.js
@@ -2,9 +2,14 @@ function csvToTable(csv, table) {
     var lines = csv.split("\n");
     var headers = lines[0].split(",");
     var table = document.getElementById(table);
+    var header = table.createTHead();
+    var hrow = header.insertRow(0);
+    for (var i = 0; i < headers.length; i++) {
+        var hrcell = hrow.insertCell(0);
+        hrcell.innerHTML = headers[i];
+    }
     for (var i = 1; i < lines.length; i++) {
         var cells = lines[i].split(",");
-        console.log(table);
         var row = table.insertRow(-1);
         for (var j = 0; j < cells.length; j++) {
             var cell = row.insertCell(-1);
@@ -37,12 +42,9 @@ function displayDates(year) {
 var tables = document.getElementsByTagName("table");
 for (var i = 0; i < tables.length; i++) {
     var table = tables[i];
-    console.log(table);
     var year = table.id;
-    console.log(year);
     if (year) {
         data = displayDates(year);
-        console.log(data);
         csvToTable(data, year);
     }
 }
