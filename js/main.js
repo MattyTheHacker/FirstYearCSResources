f10bc21c767283a359e38406bcdc3ca9ac202123
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2022-08-02 21:32:47 +0100

    complete re-write

--- js/main.js
+++ js/main.js
@@ -1,6 +1,40 @@
-var data = csv.toObjects("data/y1s2_sa.csv");
+const csvFile = ("data/y1s2_sa.csv");
+const reader = new FileReader();
+
+reader.onload = function (e) {
+    const text = e.target.result;
+    const data = csvToArray(text);
+    console.log(data);
+  };
+
+
+function csvToArray(str, delimiter = ",") {
+    // slice from start of text to the first \n index
+    // use split to create an array from string by delimiter
+    const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
+  
+    // slice from \n index + 1 to the end of the text
+    // use split to create an array of each csv value row
+    const rows = str.slice(str.indexOf("\n") + 1).split("\n");
+  
+    // Map the rows
+    // split values from each row into an array
+    // use headers.reduce to create an object
+    // object properties derived from headers:values
+    // the object passed as an element of the array
+    const arr = rows.map(function (row) {
+      const values = row.split(delimiter);
+      const el = headers.reduce(function (object, header, index) {
+        object[header] = values[index];
+        return object;
+      }, {});
+      return el;
+    });
+  
+    // return the array
+    return arr;
+  }
+
 const myDate = new Date();
-
-
 console.log(data);
 console.log(myDate);
