9b60e226b9d4a826e6a7140d220e8de3070c05cf
MattyTheHacker <18513864+MattyTheHacker@users.noreply.github.com>
2022-08-02 21:33:40 +0100

    add logging for debug

--- js/main.js
+++ js/main.js
@@ -14,7 +14,9 @@ function csvToArray(str, delimiter = ",") {
     return el;
   });
   arr.forEach(function (element) {
+    console.log(element['"Released"']);
     element['"Released"'] = Date.parse(element['"Released"']);
+    console.log(element['"Released"']);
     element['"Due"'] = Date.parse(element['"Due"']);
     element['"Marking Deadline"'] = Date.parse(element['"Marking Deadline"'])
   })
